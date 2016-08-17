57feb717d3010fbeac062b8e4eeb4e25d3a3fc18
Dangerous <857911782@qq.com>
2016-08-17 17:30:15 +0800

    right bar

--- js/index.js
+++ js/index.js
@@ -5,10 +5,12 @@
 $(document).ready(function() {
   // alert($('.map3d').attr('data-full'));
 
+  // 三维地图区全屏
   $('.map3d').click(function(event) {
     $(this).toggleClass('full-screen');
   });
 
+  // 搜索
   $('.search-box>form').submit(function(event) {
     // TODO: 地址查找，加载结果
 
@@ -16,6 +18,7 @@ $(document).ready(function() {
     return false;
   });
 
+  // 点击搜索框的叉号
   $('form>button').click(function(event) {
     if($('form>input').val() === ''){
       $('.search-result').slideUp(function(){
@@ -24,6 +27,7 @@ $(document).ready(function() {
     }
   });
 
+  // 点击搜索按钮
   $('.search-btn').click(function(event) {
     if($('form>input').val() === ''){
       $('.search').removeClass('unexpanded');
@@ -32,8 +36,16 @@ $(document).ready(function() {
     $('.search-box>form').submit();
   });
 
+  // 选择搜索结果项
   $('.result-list').on('click', '.list-item', function(event) {
     $(this).addClass('selected-item').siblings().removeClass('selected-item');
+  });
+
+  // 选中右侧工具栏的一项
+  $('.right-bar>div').click(function(event) {
+    $(this).addClass('selected-tool').siblings().removeClass('selected-tool');
+
+    $('.map2d>.monitor-panel').animate({right: '70px'}, 500);
   });
 
 });
